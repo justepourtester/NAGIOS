bbad4821a7cda63e07ab5be89c602ca7e4c988d7
justepourtester <justepourtestergithub@yopmail.com>
2016-11-08 15:18:22 +0100

    Update install_nagios.sh

--- install_nagios.sh
+++ install_nagios.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 apt-get install wget build-essential apache2 php apache2-mod-php7.0 php-gd libgd-dev unzip
-wget --no-check-certificate -c http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-4.2.0.tar.gz
+wget --no-check-certificate -c https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.2.0.tar.gz
+wget --no-check-certificate -c http://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz 
+useradd nagios
+groupadd nagcmd
+usermod -a -G nagcmd nagios4.2.0.tar.gz
 wget --no-check-certificate -c http://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz 
 useradd nagios
 groupadd nagcmd
